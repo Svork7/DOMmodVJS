4d77107a0e41093bb1666bce60a0d3ab8ce29202
Eugene Mikhailov <svork@ya.ru>
2022-10-17 20:49:18 +0300

    props

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-const App = () => {
-  const [btnText, setBtnText] = React.useState('Click me');
-
+const App = ({ initBtnText, initClassesList }) => {
+  const [btnText, setBtnText] = React.useState(initBtnText);
+  const [classesList, setClassesList] = React.useState(initClassesList);
   const btnClick = () => {
-    btnText = 'Hello from React';
+    setBtnText(`Hello from React ${Math.random()}`);
+    setClassesList('btn-color');
   };
+
   return (
     <div className="app">
-      <button onClick={btnClick}>{btnText}</button>
+      <button className={classesList} onClick={btnClick}>
+        {btnText}
+      </button>
     </div>
   );
 };
 
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<App initBtnText="Click me please" initClassesList="" />);
